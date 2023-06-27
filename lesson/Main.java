5947c880194233b9f7f269ab9ca5b9cf84c91377
denisganiev3354 <denisganiev3354@gmail.com>
2023-06-27 11:51:41 +0400

    new Java

---
+++ lesson/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+    public class Main
+    {
+        // public static void main(String[]args) {
+        //     int n = 5;
+        //     String a = "Hello";
+        //     var x = 'f';
+        //     boolean flag = true;
+        //     System.out.printIn(a + "World");
+        //     System.out.printIn("Hello World");
+        //     for (int i = 0; i <= 10; i++){
+        //         System.out.printIn(i);
+        //     }
+        //     while(n < 20){
+        //         System.out.printIn(n);
+        //         n+=5;
+        //     }
+
+        //     int j = 7;
+        //     do{
+        //         System.out.printIn(j);
+        //         j--;
+        //     }
+        //     while (j > 0);
+            
+        //     int[] nums = new int[5];
+        //     int[] x1 = {1,656,6,535,66,56,,65,6,5}
+        //     System.out.printIn(x1[3`])
+        //     for (int i = 0;i < x1.lenght;i++ ){
+        //         if(x1[i] %2 == 0)
+        //         System.out.printIn(x1[i])
+        //     }
+        //     Scanner in  = new Scanner(System.in)
+        //     String name = in.nextLine();
+        //     System.out.printIn(name)
+
+    
+        }
+    }
